7cf72d2eedf31cb2e2a3cba7a4b8b36b8b6f32ad
caren <carenoliveirasilvapontes@gmail.com>
2023-08-14 18:22:14 -0300

    feat:adicionando cardapio e forma de pagamento para utilizar na lógica

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,4 +1,17 @@
 class CaixaDaLanchonete {
+    constructor() {
+        this.cardapio = [
+            { codigo: 'chantily', descricao: 'Chantily (extra do Café)', valor: 1.50 },
+            { codigo: 'queijo', descricao: 'Queijo (extra do Sanduíche)', valor: 2.00 },
+            { codigo: 'cafe', descricao: 'Café', valor: 3.00 },
+            { codigo: 'suco', descricao: 'Suco Natural', valor: 6.20 },
+            { codigo: 'sanduiche', descricao: 'Sanduíche', valor: 6.50 },
+            { codigo: 'salgado', descricao: 'Salgado', valor: 7.25 },
+            { codigo: 'combo2', descricao: '1 Café e 1 Sanduíche', valor: 7.50 },
+            { codigo: 'combo1', descricao: '1 Suco e 1 Sanduíche', valor: 9.50 }
+        ];
+        this.formaDePagamento = ['debito', 'credito', 'dinheiro'];
+    }
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
